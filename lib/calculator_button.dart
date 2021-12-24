cf72c106c1e4857ce6547df564e0903235e23f62
Kenneth Pham <kennethpham98@gmail.com>
2021-12-24 01:50:44 -0800

    First commit added all files

---
+++ lib/calculator_button.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CalculatorButton extends StatelessWidget {
+  final String text;
+  final void Function() buttonHandler;
+  CalculatorButton(this.text, this.buttonHandler);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(5),
+      child: ElevatedButton(
+        child: Text(text),
+        onPressed: buttonHandler,
+      )
+    );
+  }
+}
